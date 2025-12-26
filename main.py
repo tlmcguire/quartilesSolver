7536b823731a5ea0ab4bb7ea37b611a6d1870c02
tlmcguire <109298992+tlmcguire@users.noreply.github.com>
2025-12-26 09:43:11 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from nltk.corpus import wordnet
+
+# Tool to solve Apple News+ Quartiles game
+# Game format: 5 rows and 4 cols of 2-3 letter word fragments, which can themselves be words.
+# Goal: Combine fragments to form all possible words, in particular the 5 fragment words (of which there are only 5)
+
+
+
+words = []
+
+def check(w: str) -> bool:
+    pass
+
+def concat():
+    pass
+
+def main():
+    pass
