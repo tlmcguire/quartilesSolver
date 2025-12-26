d61a0dcc77e487d58a572d78eaa8dbd2ac23bf11
tlmcguire <109298992+tlmcguire@users.noreply.github.com>
2025-12-26 09:44:05 -0500

    Updated spacing, removed global word list

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from nltk.corpus import wordnet
 # Game format: 5 rows and 4 cols of 2-3 letter word fragments, which can themselves be words.
 # Goal: Combine fragments to form all possible words, in particular the 5 fragment words (of which there are only 5)
 
-
-
-words = []
-
 def check(w: str) -> bool:
     pass
 
